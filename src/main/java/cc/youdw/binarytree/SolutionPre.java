942a60fea2815c43c9db8295e4cbb036384ef231
youdw <eastyoudw@gmail.com>
2018-02-23 18:23:14 +0800

    first commit

---
+++ src/main/java/cc/youdw/binarytree/SolutionPre.java
@@ -0,0 +1,66 @@
+package cc.youdw.binarytree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * <P>前序排列<P>
+ *
+ * @author youdw
+ * @date 2018/2/23 下午6:17
+ **/
+public class SolutionPre {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preOrder(root, result);
+        return result;
+    }
+
+    /**
+     * 递归方式遍历
+     *
+     * @param root
+     * @param result
+     */
+    private void preOrder(TreeNode root, List<Integer> result) {
+        if (root != null) {
+            //根节点
+            result.add(root.val);
+            //左子节点
+            preOrder(root.left, result);
+            //右子节点
+            preOrder(root.right, result);
+        }
+    }
+
+    /**
+     * 迭代方式解决
+     *
+     * @param root
+     * @param result
+     */
+    private void preOrderIteratively(TreeNode root, List<Integer> result) {
+        Stack<TreeNode> nodeStack = new Stack<TreeNode>();
+        if (root == null) {
+            return;
+        }
+        nodeStack.push(root);
+        while (!nodeStack.empty()) {
+            TreeNode node = nodeStack.peek();
+
+            if (node.left == null && node.right == null) {
+                nodeStack.pop();
+            }
+
+            if (node.left != null) {
+                nodeStack.push(node.left);
+            }
+
+
+        }
+
+    }
+
+}
---
+++ src/main/java/cc/youdw/binarytree/TreeNode.java
@@ -0,0 +1,17 @@
+package cc.youdw.binarytree;
+
+/**
+ * <P><P>
+ *
+ * @author youdw
+ * @date 2018/2/23 下午6:16
+ **/
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int x) {
+        val = x;
+    }
+}
